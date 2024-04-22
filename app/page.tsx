0e0cfaf27e0278bdf8bd67c047fd72d19513cfbe
luke7231 <tmddnjs7231@gmail.com>
2024-04-22 17:05:58 +0900

    feat: tech blog 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ContentsData } from "./lib/data";
+import { ContentsData, techBlogs } from "./lib/data";
 
 export default function Home() {
   return (
@@ -34,6 +34,30 @@ export default function Home() {
             </div>
           );
         })}
+      </div>
+      <div className="flex flex-col w-full max-w-4xl mt-16">
+        <span className="font-bold text-lg border border-gray-500 pl-2 py-2 mb-6">
+          TECH BLOGS
+        </span>
+        <div className="flex flex-wrap gap-x-10 gap-y-6 pl-2 ">
+          {techBlogs.map((blog, index) => {
+            return (
+              <div key={index}>
+                <div className="font-semibold">{blog.topic}</div>
+                <div className="h-[1px] bg-gray-700 mb-2" />
+                <div>
+                  {blog.list.map((page, index) => {
+                    return (
+                      <a key={index} href={page.link} className="block">
+                        {page.title}
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </main>
   );
