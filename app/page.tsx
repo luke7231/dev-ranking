87394e3d8adc283ebb429d2f04334872ac21825e
luke7231 <tmddnjs7231@gmail.com>
2024-04-22 16:50:38 +0900

    chore: css-gap

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { ContentsData } from "./lib/data";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-4xl font-mono text-sm grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-8">
+      <div className="z-10 w-full max-w-4xl font-mono text-sm grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-10">
         {ContentsData.map((block, index) => {
           return (
             <div key={index}>
