feaf1d0654a50a4dba0d8f46385d23a62910d48e
luke7231 <tmddnjs7231@gmail.com>
2024-04-25 16:20:08 +0900

    fix: newsletter page header 너비 수정

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center px-4 lg:px-0">
       <header className="w-full max-w-4xl py-4">
-        <Link href={"./"}>
+        <Link href={"./"} className="inline-block">
           <Image alt="logo" src={"/logo.png"} width={132} height={74.25} />
         </Link>
       </header>
