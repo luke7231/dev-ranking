25f16b1d0a94713edea29f98c212566e096f32a8
luke7231 <tmddnjs7231@gmail.com>
2024-04-25 17:34:21 +0900

    feat: action error 처리

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -2,14 +2,28 @@
 
 import { sql } from "@vercel/postgres";
 
+const isValidEmail = (email: string) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
 export async function addEmail(
   currentState: { ok: boolean; message: string },
   form: FormData
 ) {
   try {
     const email = form.get("email") as string;
+
+    if (!isValidEmail(email)) {
+      return {
+        ok: false,
+        message: "옳지 않은 이메일 형식입니다.",
+      };
+    }
+
     const existedEmail =
       await sql`SELECT * FROM beta_user WHERE email = ${email}`;
+
     if (existedEmail.rowCount > 0) {
       return {
         ok: false,
